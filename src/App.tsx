7602ba9e97546bebd5986a9d13f9e9b55b728fb5
Erim <thepropecywt@gmail.com>
2026-05-18 06:12:04 +0300

    revert main flex spacing

--- src/App.tsx
+++ src/App.tsx
@@ -198,7 +198,7 @@ export default function App() {
       </div>
 
       {/* Main content */}
-      <main className="relative z-10 flex-1 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <main className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         {loading ? (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
             {Array.from({ length: 8 }).map((_, i) => (
